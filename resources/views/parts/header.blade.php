6483f7db2b4ac9a233e3564a08882dd60560911a
snidima <snidima@mail.ru>
2017-01-24 13:14:17 +0700

    tmp

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -17,8 +17,13 @@
     </div>
     <div class="menu-right">
         <ul class="menu-list">
-            <li class="menu-list__item"><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i> Войти</a></li>
-            <li class="menu-list__item"><a href="#"><i class="fa fa-user-o" aria-hidden="true"></i> Регистрация</a></li>
+            @if( Auth::check() )
+                <li class="menu-list__item"><a href="{{route('user')}}"><i class="fa fa-user-circle" aria-hidden="true"></i> Личный кабинет</a></li>
+                <li class="menu-list__item"><a href="{{route('logout')}}"><i class="fa fa-sign-out" aria-hidden="true"></i> Выйти</a></li>
+            @else
+                <li class="menu-list__item"><a href="{{route('login')}}"><i class="fa fa-sign-in" aria-hidden="true"></i> Войти</a></li>
+                <li class="menu-list__item"><a href="{{route('register')}}"><i class="fa fa-user-plus" aria-hidden="true"></i> Регистрация</a></li>
+            @endif
         </ul>
     </div>
 </nav>
